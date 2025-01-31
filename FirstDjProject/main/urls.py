083e815fdcf725b007860d4a9bbb360ad20db5ab
Lora <larachka.06@mail.ru>
2025-01-31 22:01:10 +0100

    добавила вывод на главный экран

--- FirstDjProject/main/urls.py
+++ FirstDjProject/main/urls.py
@@ -1,7 +1,9 @@
+from django.template.context_processors import request
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("index/", views.index, name="index"),
+    path("", views.home, name="home"),
 ]
--- FirstDjProject/main/views.py
+++ FirstDjProject/main/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
-
+from .models import Question
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def home(request):
+    questions = Question.objects.all()  # Получаем все вопросы
+    return render(request, 'home.html', {'questions': questions})
